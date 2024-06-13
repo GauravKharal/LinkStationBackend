c58271b729bda11c85b9e07e7b3df26d3641cf80
Gaurav Kharal <gauravkharal9@gmail.com>
2024-06-13 20:37:19 +0545

    minor user model changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,7 +24,7 @@ const userSchema = new Schema(
       trim: true,
       index: true,
     },
-    dateOfBirth:{
+    dateOfBirth: {
       type: Date,
     },
     avatar: {
@@ -53,31 +53,28 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      username: this.username
+      username: this.username,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-  )
-}
-userSchema.methods.generateRefreshToken = function(){
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
-  )
-}
-
-
-
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
