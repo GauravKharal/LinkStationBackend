5cecf188dd89e2a9ea02e028aedd06f4d1bfd55d
Gaurav Kharal <gauravkharal9@gmail.com>
2024-07-29 23:20:32 +0545

    Created Link Model

---
+++ src/models/link.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const linkSchema = new Schema(
+  {
+    station: {
+      type: Schema.Types.ObjectId,
+      ref: "Station",
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    clicks: {
+      type: Number,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Link = mongoose.model("Link", linkSchema);
